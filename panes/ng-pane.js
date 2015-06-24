ecf1b0888c311f54f0e8d03c381b0abdd68d08ee
Liujianfu <liu_kx@hotmail.com>
2015-06-23 22:53:56 -0400

    fix a bug of setting style with angular dynamically in IE

--- panes/ng-pane.js
+++ panes/ng-pane.js
@@ -12,12 +12,12 @@
         this.scope = $scope;
         this.dockSide = "right";
         $scope.options = {};//minWidth maxWidth ...
-        this.multiplePaneStyle="padding-right:35px;float: right; top:0; bottom:0; right:0;  width: 300px;" + 
-                                "background-color:#64646c;position:fixed;z-index: 10000; display:none; border: solid 1px darkgrey; box-shadow: rgba(0, 0, 0, 0.7) 0 1px 10px 0;";
-        this.spliterBarStyle = " margin-left:-3px;background-color:darkgrey; height:100%;  float: left; width: 3px;  cursor: col-resize;";
-        this.paneContainerStyle = "float: left; margin:2px;width: 100%;  height:100%; overflow: scroll; ";
-        this.paneselectBarStyle = "padding-top:15px; right: 0; top:0; bottom:0;float: right; width: 35px; height: auto;" +
-                                        " background-color: #3b3b49;position:fixed;z-index: 20001;";
+        this.multiplePaneStyle="{'padding-right':'35px','float': 'right','top':'0', 'bottom':'0','right':'0', 'width':'300px'," + 
+                                "'background-color':'#64646c','position':'fixed','z-index': '10000', 'display':'none','border': 'solid 1px darkgrey', 'box-shadow': 'rgba(0, 0, 0, 0.7) 0 1px 10px 0'}";
+        this.spliterBarStyle = "{'margin-left':'-3px','background-color':'darkgrey', 'height':'100%','float':'left','width':'3px','cursor':'col-resize'}";
+        this.paneContainerStyle = "{'float':'left','margin':'2px','width':'100%','height':'100%','overflow':'scroll'}";
+        this.paneselectBarStyle = "{'padding-top':'15px', 'right': '0', top:0, 'bottom':'0','float': 'right', 'width':'35px','height':'auto'," +
+                                        " 'background-color': '#3b3b49','position':'fixed','z-index': '20001'}";
         this.paneActive = null;
         this.panes = [{
                             hidden: false,
@@ -37,10 +37,10 @@
                         },
                         {
                             hidden: false,
-                            helpText: "Add containers",
+                            helpText: "Add new question",
                             helpTextPlacement: "left",
                             className: "pane-new-question",
-                            contentTemplate: "modules/assessmentTemplate/assessmentTemplate.addContainer.tpl.html"
+                            contentTemplate: "modules/assessmentTemplate/assessmentTemplate.newQuestion.tpl.html"
                         }
                     ];
         this.toggle = function (pane) {
@@ -113,24 +113,24 @@
                       if (scope.panesCtrl.dockSide === "left")
                       {
                           //need to change styles 
-                          scope.panesCtrl.multiplePaneStyle = "padding-left:35px;float: left; top:0; bottom:0; left:0;  width: 300px;" +
-                                                  "background-color:#64646c;position:fixed;z-index: 10000; display:none; border: solid 1px darkgrey; box-shadow: rgba(0, 0, 0, 0.7) 0 1px 10px 0;";
-                          scope.panesCtrl.spliterBarStyle = " margin-right:-3px;background-color:darkgrey; height:100%;  float: right; width: 3px;  cursor: col-resize;";
-                          scope.panesCtrl.paneContainerStyle = "float: right; margin:2px;width: 100%;  height:100%;overflow: scroll; ";
-                          scope.panesCtrl.paneselectBarStyle = "padding-top:15px; left: 0; top:0; bottom:0;float: left; width: 35px; height: auto;" +
-                                                          " background-color: #3b3b49;position:fixed;z-index: 20001;";
+                          scope.multiplePaneStyle = "{'padding-left':'35px','float': 'left','top':'0', 'bottom':'0','left':'0', 'width':'300px'," +
+                                                  "'background-color':'#64646c','position':'fixed','z-index': '10000', 'display':'none','border': 'solid 1px darkgrey', 'box-shadow': 'rgba(0, 0, 0, 0.7) 0 1px 10px 0'}";
+                          scope.spliterBarStyle = "{'margin-right':'-3px','background-color':'darkgrey', 'height':'100%','float':'right','width':'3px','cursor':'col-resize'}";
+                          scope.paneContainerStyle = "{'float':'right','margin':'2px','width':'100%','height':'100%','overflow':'scroll'}";
+                          scope.paneselectBarStyle = "{'padding-top':'15px', 'left': '0', 'top':'0', 'bottom':'0','float': 'left', 'width':'35px','height':'auto'," +
+                                                          " 'background-color': '#3b3b49','position':'fixed','z-index': '20001'}";
                       }
                   }
               },
-              template: "<div class='ui-multiplepane'  id='ui-multiplepane-pane' style='{{panesCtrl.multiplePaneStyle}}' >" +
-                            "<div id='pane-split-bar' style='{{panesCtrl.spliterBarStyle}}'></div>" +
-                            "<div id ='pane-container' style='{{panesCtrl.paneContainerStyle}}'>" +
+              template: "<div class='ui-multiplepane'  id='ui-multiplepane-pane' ng-style='{{panesCtrl.multiplePaneStyle}}' >" +
+                            "<div id='pane-split-bar' ng-style='{{panesCtrl.spliterBarStyle}}'></div>" +
+                            "<div id ='pane-container' ng-style='{{panesCtrl.paneContainerStyle}}'>" +
                                 "<div class='ui-pane' ng-show='pane==panesCtrl.paneActive' ng-repeat='pane in panesCtrl.panes'>" +
                                     "<div> <ng-include src=\"pane.contentTemplate\"/></div>" +
                                 "</div>" +
                             "</div>" +
                         "</div>" +
-                        "<div clase ='paneselect-bar' style='{{panesCtrl.paneselectBarStyle}}'>" +
+                        "<div clase ='paneselect-bar' ng-style='{{panesCtrl.paneselectBarStyle}}'>" +
 
                              "<div class='ui-paneselector' ng-repeat='pane in panesCtrl.panes' tooltip='{{pane.helpText}}' tooltip-placement='{{pane.helpTextPlacement}}'" +
                                   "ng-hide='pane.hidden' ng-class='{active:pane=== panesCtrl.paneActive}'>" +
